52383912a1a16599ac4701e6e3184e72d06e346b
adammm26 <davidadam816@gmail.com>
2026-06-26 23:44:58 +0700

    Menambahkan frontend halaman admin login

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -1,62 +1,176 @@
 @extends('layouts.app')
+@vite([
+    'resources/css/landing-page/index.css',
+    'resources/css/auth/admin-login.css'
+])
 
 @section('content')
 
-<h1>Admin Panel</h1>
+<main class="login-wrapper">
 
-<form action="{{ route('admin.login.submit') }}" method="POST">
+    <div class="login-card">
+
+        <div class="login-header">
+            <h1 class="login-title">Login ke Akun</h1>
+            <p class="login-subtitle">
+                Silakan login dulu sebelum mengakses ke halaman
+            </p>
+        </div>
+
+        <form
+            class="login-form"
+            action="{{ route('admin.login.submit') }}"
+            method="POST">
     @csrf
 
-    <div>
-        <label for="email">Email</label>
-        <input
-            type="email"
-            id="email"
-            name="email"
+    <div class="form-group">
 
-            placeholder="admin@gmail.com"
-        >
-        @error('email')
-            <span>{{ $message }}</span>
-        @enderror
+        <label class="form-label">
+            Email
+        </label>
+
+    <div class="input-wrapper">
+
+        <i class="bi bi-person-fill left-icon"></i>
+
+        <input
+             type="email"
+             id="email"
+             name="email"
+             class="form-input"
+             placeholder="Masukkan Email Anda">
+
     </div>
 
-    <div>
-        <label for="password">Password</label>
+        @error('email')
+             <span class="form-error">
+                 {{ $message }}
+             </span>
+        @enderror
+
+</div>
+
+   <div class="form-group">
+
+        <label class="form-label">
+            Password
+        </label>
+
+    <div class="input-wrapper">
+
+        <i class="bi bi-lock-fill left-icon"></i>
+
         <input
             type="password"
+            class="form-input"
             id="password"
             name="password"
-            placeholder="Masukan Password anda"
-        >
-        @error('password')
-            <span>{{ $message }}</span>
-        @enderror
+            placeholder="Masukkan Password">
+
+        <i
+           class="bi bi-eye-slash-fill right-icon"
+           id="togglePassword">
+        </i>
+
     </div>
 
-    <div>
-        <label for="password_confirmation">Ulangi Password</label>
+        @error('password')
+            <span class="form-error">
+                {{ $message }}
+            </span>
+        @enderror
+ 
+</div>
+
+   <div class="form-group">
+
+       <label class="form-label">
+           Konfirmasi Password
+       </label>
+
+    <div class="input-wrapper">
+
+        <i class="bi bi-lock-fill left-icon"></i>
+
         <input
             type="password"
             id="password_confirmation"
             name="password_confirmation"
-            placeholder="Ulangi password Anda"
-        >
-        @error('password_confirmation')
-            <span>{{ $message }}</span>
-        @enderror
+            class="form-input"
+            placeholder="Password Anda">
+
+        <i
+           class="bi bi-eye-slash-fill right-icon"
+           id="toggleConfirmPassword">
+        </i>
+
     </div>
 
-    <button type="submit">Masuk</button>
+        @error('password_confirmation')
+            <span class="form-error">
+                {{ $message }}
+            </span>
+        @enderror
+
+</div>
+
+    <div class="form-actions">
+
+    <button
+        type="submit"
+        class="btn-primary">
+
+        Masuk
+
+    </button>
+
+</div>
 
 </form>
 
 @if ($errors->any())
-    <ul>
+    <ul class="error-list">
         @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
         @endforeach
     </ul>
 @endif
 
+<script>
+
+const togglePassword = document.getElementById('togglePassword');
+const password = document.getElementById('password');
+
+togglePassword.addEventListener('click', function () {
+
+    const type = password.getAttribute('type') === 'password'
+        ? 'text'
+        : 'password';
+
+    password.setAttribute('type', type);
+
+    this.classList.toggle('bi-eye-fill');
+    this.classList.toggle('bi-eye-slash-fill');
+
+});
+
+const toggleConfirmPassword = document.getElementById('toggleConfirmPassword');
+const confirmPassword = document.getElementById('password_confirmation');
+
+toggleConfirmPassword.addEventListener('click', function () {
+
+    const type = confirmPassword.getAttribute('type') === 'password'
+        ? 'text'
+        : 'password';
+
+    confirmPassword.setAttribute('type', type);
+
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+
+});
+
+</script>
+
 @endsection
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Booking</title>
+
+    <link
+rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 @vite(['resources/css/layouts/index.css'])
 
